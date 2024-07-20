cad3f7b151772d579e6f3c127d8519f3693ff191
Ashu <Intermezzobrilliance@gmail.com>
2024-07-20 12:27:44 +0530

    day3

---
+++ Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays {
+    public static void main(String[] args) {
+        //for each loop 
+        // for (int i:arr) where arr is iterable object
+       
+        int[] arr=new int[4];
+        
+        for (int i :arr){
+            System.err.println(i);
+
+        }
+    }
+}
